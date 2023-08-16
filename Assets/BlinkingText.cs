cc0e5a831bbeeb80d401f88f41ddaade040ead58
igrodeltab <igrodeltab@gmail.com>
2023-08-16 23:55:23 +0400

    add blinking effect for text

---
+++ Assets/BlinkingText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class BlinkingText : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text; // Ссылка на компонент текста
+    [SerializeField] private float _blinkSpeed; // Скорость мерцания
+
+    private bool _isVisible = true; // Флаг для отслеживания видимости текста
+    private float _timer; // Таймер для мерцания
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= 1f / _blinkSpeed) // Если прошло достаточно времени для мигания
+        {
+            _isVisible = !_isVisible; // Инвертируем видимость текста
+            _text.enabled = _isVisible; // Применяем измененную видимость к тексту
+            _timer = 0f; // Сбрасываем таймер
+        }
+    }
+}
